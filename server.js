71c5a828e6b58ec5c11dbe433db2b80dfa4cbe9c
mohamed el-maghraby <mohamed el-maghraby>
2017-04-29 09:05:07 -0700

    setting up heroku configuration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -46,4 +47,4 @@ app.get('/bad', (req, res) => {
         error: 'unable to handle request'
     });
 })
-app.listen(3000);
+app.listen(port);
